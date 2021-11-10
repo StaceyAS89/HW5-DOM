e7e2b8e41519a9e7435a9a77a319a311cde2a348
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-10 17:45:20 +0200

    last commit

--- test.js
+++ test.js
@@ -1,24 +1,17 @@
 
 let usernameValue = document.getElementById('username');
 let passwordValue = document.getElementById('password');
-// let loginData = usernameValue + passwordValue;
-// let loginButton = document.getElementById('login');
 
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    document.getElementById('enteredValues').innerHTML =
+        'Username: ' +
+        usernameValue.value +
+        '   ' +
+        'Password: ' +
+        passwordValue.value;
 
-document.getElementById('loginForm').addEventListener('submit', function(e){
-    e.preventDefault(); 
-    document.getElementById('enteredValues').innerHTML = 'Username: '+ usernameValue.value + '   '+'Password: ' + passwordValue.value;
-}
-)
-// let usernameData =  usernameValue.innerHTML;
-// let passwordData = passwordValue.value;
+        localStorage.setItem('username', usernameValue.value);
+        localStorage.setItem('password', passwordValue.value);
 
-
-// localStorage.setItem('username', usernameValue.value);
-
-document.getElementById('loginForm').addEventListener('submit', function(e){
-    e.preventDefault(); 
-    localStorage.setItem('username', usernameValue.value);
-    localStorage.setItem('password', passwordValue.value);
-}
-)
+});
